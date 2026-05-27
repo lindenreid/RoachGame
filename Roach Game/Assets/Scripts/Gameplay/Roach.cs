5a752e87c083346fbcb55ee43c5909db7659e0b1
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2026-05-26 18:32:33 -0700

    roachie good movement

--- Roach Game/Assets/Scripts/Gameplay/Roach.cs
+++ Roach Game/Assets/Scripts/Gameplay/Roach.cs
@@ -24,17 +24,14 @@ public class Roach : NPC
     // ------------------------------------------------------------------------
     [SerializeField] private float _runSpeed = 0.01f;
     [SerializeField] private Vector2 _idleTimeMinMax;
-    [SerializeField] private SplineContainer _runningSplines;
+    [SerializeField] private SplineAnimate _splineAnimator;
+    [SerializeField] private Transform _roachSplines;
 
     private RoachState _currentState;
     private float _stateTime;
 
     // idle state
     private float _maxStateTime;
-
-    // running state
-    private Spline _currentSpline;
-    private Vector3 _startPos;
 
     // ------------------------------------------------------------------------
     // Methods
@@ -68,11 +65,11 @@ public class Roach : NPC
     {
         _currentState = RoachState.Running;
 
-        _currentSpline = _runningSplines.Splines[
-            Random.Range(0, _runningSplines.Splines.Count-1)
-        ];
-        _stateTime = 0;
-        _startPos = transform.position;
+        transform.Rotate(0, Random.Range(0, 350), 0);
+        _roachSplines.Rotate(0, Random.Range(0, 350), 0);
+        _roachSplines.position = transform.position;
+
+        _splineAnimator.Restart(true);
     }
 
     // ------------------------------------------------------------------------
@@ -88,11 +85,7 @@ public class Roach : NPC
     // ------------------------------------------------------------------------
     private void DoRunningState ()
     {
-        _stateTime += Time.deltaTime * _runSpeed;
-
-        transform.position = _startPos + (Vector3)SplineUtility.EvaluatePosition(_currentSpline, _stateTime);
-
-        if(_stateTime >= 1)
+        if(!_splineAnimator.IsPlaying)
         {
             EnterIdleState();
         }
