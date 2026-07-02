cff886003709d96615a87378ac0cf56709bd0265
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2026-07-01 21:21:53 -0700

    fade in when game starts

--- Roach Game/Assets/Scripts/Cinematics/PostEffectController.cs
+++ Roach Game/Assets/Scripts/Cinematics/PostEffectController.cs
@@ -18,32 +18,53 @@ public class PostEffectController : MonoBehaviour
     // Variables
     // ------------------------------------------------------------------------
     [SerializeField] public Volume _ppVolume;
+    [SerializeField] public int _fadeInDurationFrames = 120;
 
     private ColorAdjustments _colorAdjustments;
+    private bool _doFadeIn;
+    private int _fadeTimeFrames;
 
     // ------------------------------------------------------------------------
     // Methods
     // ------------------------------------------------------------------------
-    public void Start()
+    private void Start()
     {
         _ppVolume.profile.TryGet<ColorAdjustments>(out _colorAdjustments);
         Assert.IsNotNull(_colorAdjustments);
 
-        SetFadeInAlpha(0.0f);
+        SetFadeInValue(1.0f);
+    }
+
+    // ------------------------------------------------------------------------
+    private void Update()
+    {
+        if(_doFadeIn)
+        {
+            _fadeTimeFrames += 1;
+
+            SetFadeInValue((float)_fadeTimeFrames / (float)_fadeInDurationFrames);
+
+            if(_fadeTimeFrames >= _fadeInDurationFrames)
+            {
+                SetFadeInValue(1.0f);
+                _doFadeIn = false;
+            }
+        }
     } 
 
     // ------------------------------------------------------------------------
     // timeline signal callback
     public void StartFadeIn ()
     {
-        
+        _doFadeIn = true;
+        _fadeTimeFrames = 0;
     }
 
     // ------------------------------------------------------------------------
-    private void SetFadeInAlpha(float a)
+    private void SetFadeInValue(float t)
     {
         _colorAdjustments.colorFilter.SetValue(new ColorParameter(
-            Color.Lerp(Color.white, Color.black, a),
+            Color.Lerp(Color.black, Color.white, t),
             true
         ));
     }
