cefff6fb6eec83daee225b8800e308377a95a8a0
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2026-07-01 20:35:04 -0700

    reorganized and added pp controller

---
+++ Roach Game/Assets/Scripts/Cinematics/PostEffectController.cs
@@ -0,0 +1,28 @@
+/*
+ * Filename: /Users/lindenreid/Documents/GitHub/RoachGame/Roach Game/Assets/Scripts/Cinematics/PostEffectController.cs
+ * Path: /Users/lindenreid/Documents/GitHub/RoachGame/Roach Game/Assets/Scripts/Cinematics
+ * Created Date: Wednesday, July 1st 2026, 5:54:45 pm
+ * Author: Travis Reid
+ * 
+ * Copyright (c) 2026 Studio Tilia
+ */
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PostEffectController : MonoBehaviour
+{
+    // ------------------------------------------------------------------------
+    // Variables
+    // ------------------------------------------------------------------------
+    [SerializeField] public Volume _ppVolume;
+
+    // ------------------------------------------------------------------------
+    // Methods
+    // ------------------------------------------------------------------------
+    // timeline signal callback
+    public void StartFadeIn ()
+    {
+        
+    }
+}
