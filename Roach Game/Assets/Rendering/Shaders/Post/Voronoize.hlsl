547d0aeecf8850a79c440617770fefd1a154357f
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2026-07-28 22:48:08 -0700

    wrote voronoi alg

--- Roach Game/Assets/Rendering/Shaders/Post/Voronoize.hlsl
+++ Roach Game/Assets/Rendering/Shaders/Post/Voronoize.hlsl
@@ -2,8 +2,36 @@
 #ifndef MYHLSLINCLUDE_INCLUDED
 #define MYHLSLINCLUDE_INCLUDED
 
-void Voronoize_float(float2 UV, float Speed, float Density, out float3 Out)
+// thank you https://sangillee.com/_posts/2025-04-18-Cellular-noises/
+
+float3 random3 (float2 p)
 {
-    Out = float3(1,1,1);
+    float3 q = float3( dot(p, float2(127.1,311.7)), 
+        dot(p, float2(269.5,183.3)), 
+        dot(p, float2(419.2,371.9)) );
+    return frac(sin(q) * 43758.5453);
+}
+
+void Voronoize_float(float2 UV, float Speed, float Density, out float Noise)
+{
+    float2 value = UV * Density;
+    
+    float2 i_st = floor(value);
+    float2 f_st = frac(value);
+
+    float minDist = 100;
+
+    for(int y = -1; y <= 1; y++)
+    {
+        for(int x = -1; x <= 1; x++)
+        {
+            float2 neighbor = float2(x, y);
+            float2 pt = random3(i_st + neighbor).xy;
+            float dist = length(neighbor + pt - f_st);
+            minDist = min(minDist, dist);
+        }
+    }
+
+    Noise = minDist;
 }
 #endif //MYHLSLINCLUDE_INCLUDED
