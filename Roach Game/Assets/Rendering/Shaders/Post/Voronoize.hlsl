29cb988d2ec1637d51ceb4b974397f558e7045a3
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2026-07-28 22:17:09 -0700

    made voronoi into custom shader node

---
+++ Roach Game/Assets/Rendering/Shaders/Post/Voronoize.hlsl
@@ -0,0 +1,9 @@
+//UNITY_SHADER_NO_UPGRADE
+#ifndef MYHLSLINCLUDE_INCLUDED
+#define MYHLSLINCLUDE_INCLUDED
+
+void Voronoize_float(float2 UV, float Speed, float Density, out float3 Out)
+{
+    Out = float3(1,1,1);
+}
+#endif //MYHLSLINCLUDE_INCLUDED
