9f4b3d9099726be653af4d8ce0219fa42b444e54
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2026-07-29 23:10:26 -0700

    triplanar voronoi heheheh

--- Roach Game/Assets/Rendering/Shaders/Shared/MathHelp.hlsl
+++ Roach Game/Assets/Rendering/Shaders/Shared/MathHelp.hlsl
@@ -2,6 +2,12 @@
 #ifndef REID_MATHHELP_INCLUDED
 #define REID_MATHHELP_INCLUDED
 
+void TriplanarWeights_float(float3 normal, out float3 weights)
+{
+    float3 triW = abs(normal);
+	weights = triW / (triW.x + triW.y + triW.z);
+}
+
 void SinTime01_float(float Time, float Speed, out float Result)
 {
     Result = sin(Time*Speed)*0.5 + 0.5;
