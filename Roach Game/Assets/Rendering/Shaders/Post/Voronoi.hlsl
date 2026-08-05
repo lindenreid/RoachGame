b6fa31f5aca84bb6d178599ae551b7be308fd00b
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2026-08-04 22:48:30 -0700

    reduced voronoi precision to half

--- Roach Game/Assets/Rendering/Shaders/Post/Voronoi.hlsl
+++ Roach Game/Assets/Rendering/Shaders/Post/Voronoi.hlsl
@@ -9,28 +9,28 @@
 // Edited by Sangil Lee
 // Translated to HLSL and further edited by Travis Reid
 
-float random1 (float2 value, float2 dotDir = float2(12.9898, 78.233))
+half random1 (half2 value, half2 dotDir = half2(12.9898, 78.233))
 {
-    float2 smallValue = sin(value);
-    float random = dot(smallValue, dotDir);
+    half2 smallValue = sin(value);
+    half random = dot(smallValue, dotDir);
     random = frac(sin(random) * 143758.5453);
     return random;
 }
 
-float3 random3 (float2 p)
+half3 random3 (half2 p)
 {
-    float3 q = float3( dot(p, float2(127.1,311.7)), 
-        dot(p, float2(269.5,183.3)), 
-        dot(p, float2(419.2,371.9)) );
+    half3 q = half3( dot(p, half2(127.1,311.7)), 
+        dot(p, half2(269.5,183.3)), 
+        dot(p, half2(419.2,371.9)) );
     return frac(sin(q) * 43758.5453);
 }
 
-float weightedDist(float2 x, float2 y, float w)
+half weightedDist(half2 x, half2 y, half w)
 {
     return 1.0/w * length(x-y);
 }
 
-float level(float2 p)
+half level(half2 p)
 {
     if (p.x == 1.0 && p.y == 1.0)
     {
@@ -39,15 +39,15 @@ float level(float2 p)
     return 0.0;
 }
 
-float VoronoiClusters(float2 uv, float ClusterPropability, float Time, float MoveSpeed, float GrowSpeed)
+half VoronoiClusters(half2 uv, half ClusterPropability, half Time, half MoveSpeed, half GrowSpeed)
 {
     // quantize to create the tiles
-    float2 cellCoords = floor(uv);
+    half2 cellCoords = floor(uv);
 
     // big numbah
-    float minDist = 1e10;
+    half minDist = 1e10;
     
-    float offsetAmt = 2.0;
+    half offsetAmt = 2.0;
 
     // loop through self + 8 neighbos - self is at (0,0)
     // 3 nested for loops to create clusters inside individual cells
@@ -56,24 +56,24 @@ float VoronoiClusters(float2 uv, float ClusterPropability, float Time, float Mov
         for(int j = -1; j <= 1; j++)
         {
             // get distance from this pixel to neighbor cell's blob center
-            float2 neighborCoords1 = cellCoords + float2(i, j);
-            float3 cellRand = random3(neighborCoords1);
+            half2 neighborCoords1 = cellCoords + half2(i, j);
+            half3 cellRand = random3(neighborCoords1);
             // must animate blob's center location BEFORE applying neighbor coords
-            float2 cellRandCenter = 0.5 + 0.5 * sin(Time * MoveSpeed + 6.2831 * cellRand);
-            float2 blobCenter = neighborCoords1 + cellRandCenter.xy;
+            half2 cellRandCenter = 0.5 + 0.5 * sin(Time * MoveSpeed + 6.2831 * cellRand);
+            half2 blobCenter = neighborCoords1 + cellRandCenter.xy;
 
             // animate size of blob by factoring a weight into the distance equation
             //      and animating the weight value
             // the random start offset is based off of a neighboring cell's random value
             //      because i'm running out of random values i can generate from one 2d position lol
-            float weight = random1(neighborCoords1) + 0.8;
-            float randomStartOffset = random1(neighborCoords1 - float2(1,1));
+            half weight = random1(neighborCoords1) + 0.8;
+            half randomStartOffset = random1(neighborCoords1 - half2(1,1));
             weight = weight + sin(randomStartOffset*offsetAmt + Time * GrowSpeed) * weight/6.0;
-            float dist = weightedDist(blobCenter, uv, weight);
+            half dist = weightedDist(blobCenter, uv, weight);
 
             // random value to determine if this cell should create a cluster
             // keep original random value- NOT animated- as the chance of clustering should remain the same over time
-            float cluster = cellRand.z;
+            half cluster = cellRand.z;
 
             if(cluster < 1.0 - ClusterPropability)
             {
@@ -87,7 +87,7 @@ float VoronoiClusters(float2 uv, float ClusterPropability, float Time, float Mov
                 {
                     for(int l = 0; l <= 1; l++)
                     {
-                        float2 neighborCoords2 = neighborCoords1 + float2(k, l)/2.0;
+                        half2 neighborCoords2 = neighborCoords1 + half2(k, l)/2.0;
                         cellRand = random3(neighborCoords2);
                         cellRandCenter = 0.5 + 0.5 * sin(Time * MoveSpeed + 6.2831 * cellRand);
                         blobCenter = neighborCoords2 + cellRandCenter.xy/2.0;
@@ -95,7 +95,7 @@ float VoronoiClusters(float2 uv, float ClusterPropability, float Time, float Mov
                         // weight inner clusters to be smaller
                         // cluster 1 min is 0.5, max is 0.8
                         weight = random1(neighborCoords2) * 0.8 + 0.5;
-                        randomStartOffset = random1(neighborCoords2 - float2(1,1));
+                        randomStartOffset = random1(neighborCoords2 - half2(1,1));
                         weight = weight + sin(randomStartOffset*offsetAmt + Time * GrowSpeed) * weight/4.0;
                         dist = weightedDist(blobCenter, uv, weight);
 
@@ -111,13 +111,13 @@ float VoronoiClusters(float2 uv, float ClusterPropability, float Time, float Mov
                             {
                                 for(int m = 0; m <= 1; m++)
                                 {
-                                    float2 neighborCoords3 = neighborCoords2 + float2(m, n)/4.0;
+                                    half2 neighborCoords3 = neighborCoords2 + half2(m, n)/4.0;
                                     cellRand = random3(neighborCoords3);
                                     cellRandCenter = 0.5 + 0.5 * sin(Time * MoveSpeed + 6.2831 * cellRand);
                                     blobCenter = neighborCoords3 + cellRandCenter.xy/4.0;
 
                                     weight = random1(neighborCoords3) * 0.5 + 0.2;
-                                    randomStartOffset = random1(neighborCoords3 - float2(1,1));
+                                    randomStartOffset = random1(neighborCoords3 - half2(1,1));
                                     weight = weight + sin(randomStartOffset*offsetAmt + Time * GrowSpeed) * weight/4.0;
                                     dist = weightedDist(blobCenter, uv, weight);
 
@@ -136,32 +136,32 @@ float VoronoiClusters(float2 uv, float ClusterPropability, float Time, float Mov
     return minDist;
 }
 
-void VoronoiClusters_float(float2 UV, float Time, float MoveSpeed, float GrowSpeed, float Density, float ClusterPropability, out float Noise)
+void VoronoiClusters_half(half2 UV, half Time, half MoveSpeed, half GrowSpeed, half Density, half ClusterPropability, out half Noise)
 {
-    float2 v = UV * Density;
+    half2 v = UV * Density;
     Noise = VoronoiClusters(v, ClusterPropability, Time, MoveSpeed, GrowSpeed);
 }
 
-void VoronoiWeighted_float(float2 UV, float Time, float MoveSpeed, float Density, out float Noise)
+void VoronoiWeighted_half(half2 UV, half Time, half MoveSpeed, half Density, out half Noise)
 {
-    float2 value = UV * Density;
+    half2 value = UV * Density;
     
-    float2 i_st = floor(value);
-    float2 f_st = frac(value);
+    half2 i_st = floor(value);
+    half2 f_st = frac(value);
 
-    float minDist = 100;
+    half minDist = 100;
 
     for(int y = -2; y <= 2; y++)
     {
         for(int x = -2; x <= 2; x++)
         {
-            float2 neighborCoords = float2(x, y);
+            half2 neighborCoords = half2(x, y);
 
-            float2 blobCenter = random3(i_st + neighborCoords).xy;
+            half2 blobCenter = random3(i_st + neighborCoords).xy;
             blobCenter = 0.5 + 0.5 * sin(Time * MoveSpeed + 6.2831 * blobCenter);
 
-            float weight = 0.2 + 0.8 * random3(i_st + neighborCoords).z;
-            float dist = weightedDist(neighborCoords + blobCenter, f_st, weight);
+            half weight = 0.2 + 0.8 * random3(i_st + neighborCoords).z;
+            half dist = weightedDist(neighborCoords + blobCenter, f_st, weight);
 
             minDist = min(minDist, dist);
         }
@@ -170,19 +170,19 @@ void VoronoiWeighted_float(float2 UV, float Time, float MoveSpeed, float Density
     Noise = minDist;
 }
 
-void VoronoiSimple_float(float2 UV, float Time, float MoveSpeed, float Density, out float Noise)
+void VoronoiSimple_half(half2 UV, half Time, half MoveSpeed, half Density, out half Noise)
 {
     // UV is 0-1 value, so make it larger based on how many tiles we want to have
-    float2 value = UV * Density;
+    half2 value = UV * Density;
     
     // quantize to create the tiles
-    float2 cellCoords = floor(value);
+    half2 cellCoords = floor(value);
 
     // get this pixel's exact position inside the cell
-    float2 innerCellPos = frac(value);
+    half2 innerCellPos = frac(value);
 
     // big numbah
-    float minDist = 1e10;
+    half minDist = 1e10;
 
     // loop through self + 8 neighbors (9 cells total)
     // self is at (0,0)
@@ -191,11 +191,11 @@ void VoronoiSimple_float(float2 UV, float Time, float MoveSpeed, float Density, 
         for(int x = -1; x <= 1; x++)
         {
             // get coordinates for neighbor tile
-            float2 neighborCoords = float2(x, y);
+            half2 neighborCoords = half2(x, y);
 
             // get center of tile
             // center is a randomized value using the UV as the seed
-            float2 blobCenter = random3(cellCoords + neighborCoords).xy;
+            half2 blobCenter = random3(cellCoords + neighborCoords).xy;
 
             // animate center of blob based on time 
             // for some reason, _Time isn't working properly inside this custom ShaderGraph function,
@@ -203,7 +203,7 @@ void VoronoiSimple_float(float2 UV, float Time, float MoveSpeed, float Density, 
             blobCenter = 0.5 + 0.5 * sin(Time * MoveSpeed + 6.2831 * blobCenter);
 
             // get the distance from this pixel to the neighboring blob center
-            float dist = length(neighborCoords + blobCenter - innerCellPos);
+            half dist = length(neighborCoords + blobCenter - innerCellPos);
 
             // keep the closest distance value from all cell comparisons
             minDist = min(minDist, dist);
--- Roach Game/Assets/Rendering/Shaders/Shared/MathHelp.hlsl
+++ Roach Game/Assets/Rendering/Shaders/Shared/MathHelp.hlsl
@@ -5,42 +5,42 @@
 // god bless IQ as usual
 // biplanar and triplanar mapping algorithms referenced from https://iquilezles.org/articles/biplanar/
 
-void BiplanarWeights_float(float3 normal, float3 objPos, float blend, out float2 uvA, out float2 uvB, out float2 weights)
+void BiplanarWeights_half(half3 normal, half3 objPos, half blend, out half2 uvA, out half2 uvB, out half2 weights)
 {
     normal = abs(normal);
 
     // axes are used for creating UVs for texture sampling
-    float3 majorAxis = (normal.x > normal.y && normal.x > normal.z) ? float3(0, 1, 2) :
-                       (normal.y > normal.z)                        ? float3(1, 2, 0) :
-                                                                      float3(2, 0, 1) ;
+    half3 majorAxis = (normal.x > normal.y && normal.x > normal.z) ? half3(0, 1, 2) :
+                       (normal.y > normal.z)                        ? half3(1, 2, 0) :
+                                                                      half3(2, 0, 1) ;
 
-    float3 minorAxis = (normal.x < normal.y && normal.x < normal.z) ? float3(0, 1, 2) :
-                       (normal.y < normal.z)                        ? float3(1, 2, 0) :
-                                                                      float3(2, 0, 1) ;   
+    half3 minorAxis = (normal.x < normal.y && normal.x < normal.z) ? half3(0, 1, 2) :
+                       (normal.y < normal.z)                        ? half3(1, 2, 0) :
+                                                                      half3(2, 0, 1) ;   
 
-    float3 medianAxis = float3(3, 3, 3) - minorAxis - majorAxis;
+    half3 medianAxis = half3(3, 3, 3) - minorAxis - majorAxis;
     
-    uvA = float2(objPos[majorAxis.y], objPos[majorAxis.z]);
-    uvB = float2(objPos[medianAxis.y], objPos[medianAxis.z]);
+    uvA = half2(objPos[majorAxis.y], objPos[majorAxis.z]);
+    uvB = half2(objPos[medianAxis.y], objPos[medianAxis.z]);
 
     // weights are used for blending texture samples
-    weights = float2(normal[majorAxis.x], normal[medianAxis.x]);
+    weights = half2(normal[majorAxis.x], normal[medianAxis.x]);
     weights = clamp((weights - 0.5773)/(1.0 - 0.5773), 0.0, 1.0);
-    weights = pow(weights, float2(blend/8.0, blend/8.0));
+    weights = pow(weights, half2(blend/8.0, blend/8.0));
 }
 
-void BiplanarBlending_float(float3 colorA, float3 colorB, float2 weights, out float3 outColor)
+void BiplanarBlending_half(half3 colorA, half3 colorB, half2 weights, out half3 outColor)
 {
     outColor = (colorA * weights.x + colorB * weights.y) / (weights.x + weights.y);
 }
 
-void TriplanarWeights_float(float3 normal, out float3 weights)
+void TriplanarWeights_half(half3 normal, out half3 weights)
 {
-    float3 triW = abs(normal);
+    half3 triW = abs(normal);
 	weights = triW / (triW.x + triW.y + triW.z);
 }
 
-void SinTime01_float(float Time, float Speed, out float Result)
+void SinTime01_half(half Time, half Speed, out half Result)
 {
     Result = sin(Time*Speed)*0.5 + 0.5;
 }
